4f2e7cfc3e5a772998244380660316e067f5f7a6
wardziniak <bwardziniak@yahoo.pl>
2016-11-30 23:21:05 +0100

    empty method implementation

--- src/main/scala/com/wardziniak/kafka/connect/email/EmailSinkConnector.scala
+++ src/main/scala/com/wardziniak/kafka/connect/email/EmailSinkConnector.scala
@@ -6,6 +6,7 @@ import org.apache.kafka.common.config.ConfigDef
 import org.apache.kafka.connect.connector.Task
 import org.apache.kafka.connect.sink.SinkConnector
 import org.slf4j.{Logger, LoggerFactory}
+import collection.JavaConverters._
 
 /**
   * Created by wardziniak on 29.11.2016.
@@ -14,15 +15,18 @@ class EmailSinkConnector extends SinkConnector {
 
   val log: Logger = LoggerFactory.getLogger(classOf[EmailSinkConnector])
 
+  private val configProps: util.Map[String, String] = new util.HashMap[String, String]()
+
   override def taskClass(): Class[_ <: Task] = classOf[EmailSinkTask]
 
-  override def taskConfigs(maxTasks: Int): util.List[util.Map[String, String]] = ???
+  override def taskConfigs(maxTasks: Int): util.List[util.Map[String, String]] =
+    (1 to maxTasks).map(_ => configProps).asJava
 
-  override def stop(): Unit = ???
+  override def stop(): Unit = log.debug("stop")
 
-  override def config(): ConfigDef = ???
+  override def config(): ConfigDef = new ConfigDef()
 
-  override def start(props: util.Map[String, String]): Unit = ???
+  override def start(props: util.Map[String, String]): Unit = log.debug("start")
 
   override def version(): String = "0.1.1"
 }
--- src/main/scala/com/wardziniak/kafka/connect/email/EmailSinkTask.scala
+++ src/main/scala/com/wardziniak/kafka/connect/email/EmailSinkTask.scala
@@ -6,6 +6,7 @@ import org.apache.kafka.clients.consumer.OffsetAndMetadata
 import org.apache.kafka.common.TopicPartition
 import org.apache.kafka.connect.sink.{SinkRecord, SinkTask}
 import org.slf4j.{Logger, LoggerFactory}
+import collection.JavaConverters._
 
 /**
   * Created by wardziniak on 29.11.2016.
@@ -14,13 +15,15 @@ class EmailSinkTask extends SinkTask {
 
   val log: Logger = LoggerFactory.getLogger(classOf[EmailSinkTask])
 
-  override def stop(): Unit = ???
+  override def stop(): Unit = log.error("stop");
 
-  override def put(records: util.Collection[SinkRecord]): Unit = ???
+  override def put(records: util.Collection[SinkRecord]): Unit = {
+    records.asScala.foreach(record => log.error(record.value().toString))
+  }
 
-  override def flush(offsets: util.Map[TopicPartition, OffsetAndMetadata]): Unit = ???
+  override def flush(offsets: util.Map[TopicPartition, OffsetAndMetadata]): Unit = log.debug("flush")
 
-  override def start(props: util.Map[String, String]): Unit = ???
+  override def start(props: util.Map[String, String]): Unit = log.debug("start")
 
-  override def version(): String = ???
+  override def version(): String = "0.0.1"
 }
